a02adb7bc85a160c9f3f6be82a11ec82a2b9305d
luiscadari <luiscadari@icloud.com>
2025-05-20 13:19:56 -0300

    :sparkles: feat: Pet types

--- src/controller/pet.controller.ts
+++ src/controller/pet.controller.ts
@@ -1,11 +1,14 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 
-let pets = [];
+import type Pet from "../types/pet.type";
+
+let pets: Pet[] = [];
 
 export default class PetController {
-    criaPet = (req: Request, res: Response) => {
-        const newPet = req.body;
-        pets.push(newPet);
-        res.status(200).json(newPet);
-    }
+  criaPet = (req: Request, res: Response) => {
+    const { adopted, age, id, name, species } = req.body as Pet;
+    const newPet: Pet = { adopted, age, id, name, species };
+    pets.push(newPet);
+    res.status(200).json(newPet);
+  };
 }
---
+++ src/types/pet.type.ts
@@ -0,0 +1,9 @@
+type Pet = {
+  id: number;
+  name: string;
+  species: string;
+  adopted: boolean;
+  age: number;
+};
+
+export default Pet;
