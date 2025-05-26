c0c6b027d72c2649498805efabc1a0baaf4e12f1
luiscadari <luiscadari@icloud.com>
2025-05-26 07:25:19 -0300

    :sparkles: feat: pet crud

--- src/controller/pet.controller.ts
+++ src/controller/pet.controller.ts
@@ -6,9 +6,33 @@ let pets: Pet[] = [];
 
 export default class PetController {
   criaPet = (req: Request, res: Response) => {
-    const { adopted, age, id, name, species } = req.body as Pet;
+    const { id, name, species, age, adopted } = req.body as Pet;
     const newPet: Pet = { adopted, age, id, name, species };
     pets.push(newPet);
     res.status(200).json(newPet);
+  };
+  getPets = (req: Request, res: Response) => {
+    res.status(200).json(pets);
+  };
+  patchPet = (req: Request, res: Response) => {
+    const { id } = req.params;
+    const petIndex = pets.findIndex((pet) => pet.id === Number(id));
+    if (petIndex === -1) {
+      res.status(404).json({ message: "Pet not found" });
+      return;
+    }
+    const newPet = req.body as Pet;
+    pets[petIndex] = { ...newPet };
+    res.status(200).json(newPet);
+  };
+  deletePet = (req: Request, res: Response) => {
+    const { id } = req.params;
+    const petIndex = pets.findIndex((pet) => pet.id === Number(id));
+    if (petIndex === -1) {
+      res.status(404).json({ message: "Pet not found" });
+      return;
+    }
+    pets.splice(petIndex, 1);
+    res.status(204).send();
   };
 }
--- src/routes/pet.router.ts
+++ src/routes/pet.router.ts
@@ -6,6 +6,9 @@ const router = express.Router();
 
 const petController = new PetController();
 
-router.post('/', petController.criaPet);
+router.post("/", petController.criaPet);
+router.get("/", petController.getPets);
+router.put("/:id", petController.patchPet);
+router.delete("/:id", petController.deletePet);
 
 export default router;
