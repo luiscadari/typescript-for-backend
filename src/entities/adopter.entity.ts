7b6f746384ed5dae006fa383e736394d505b597a
luiscadari <luiscadari@icloud.com>
2025-06-09 07:30:55 -0300

    :sparkles: feat: Address relation
    Relacionando nova entidade de endereco com entidade de adotante

--- src/entities/adopter.entity.ts
+++ src/entities/adopter.entity.ts
@@ -1,1 +1,42 @@
-import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";@Entity()export default class AdopterEntity {    @PrimaryGeneratedColumn()    id!: number;    @Column()    name: string;    @Column()    password: string;    @Column()    phone: string;    @Column({nullable: true})    photo?: string;    @Column({nullable: true})    address?: string;    constructor(        name: string,        password: string,        phone: string,        address?: string,        photo?: string,    ) {        this.name = name;        this.password = password;        this.phone = phone;        this.address = address;        this.photo = photo;    }}
+import {Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn} from "typeorm";
+import AddressEntity from "./addressEntity";
+
+@Entity()
+export default class AdopterEntity {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    phone: string;
+
+    @Column({nullable: true})
+    photo?: string;
+
+    @OneToOne(() => AddressEntity, {
+        nullable: true,
+        cascade: true,
+        eager: true,
+    })
+    @JoinColumn()
+    address?: AddressEntity;
+
+    constructor(
+        name: string,
+        password: string,
+        phone: string,
+        address?: AddressEntity,
+        photo?: string,
+    ) {
+        this.name = name;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
+        this.photo = photo;
+    }
+}
